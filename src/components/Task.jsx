e6c8b30286adceeaf7fce6533a56ecd669b54073
MohJamal <mohjamalalkhatib96@gmail.com>
2022-08-15 01:55:18 +0300

    stop point 2

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { FaTimes } from "react-icons/fa";
 
-const Task = () => {
-  return <div>Task</div>;
+const Task = ({ task, onDelete }) => {
+  return (
+    <div className="task">
+      <h3>
+        {task.text}
+        <FaTimes
+          style={{ color: "red", cursor: "pointer" }}
+          onClick={() => onDelete(task.id)}
+        />
+      </h3>
+      <p>{task.day}</p>
+    </div>
+  );
 };
 
 export default Task;
